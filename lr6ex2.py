3d6349b3516436868af1def3388b9d3a8d4374ab
Antoshkin Ilia <iliaaantoshkin@gmail.com>
2023-11-22 11:24:00 +0300

    изменения

--- lr6ex2.py
+++ lr6ex2.py
@@ -7,11 +7,17 @@ def _split(string, deliter):
         if s[i] == deliter:
             mas += [word]
             word = ""
-        elif s[i] not in ('.,'+ deliter):
+        elif s[i] not in ('.,' + deliter):
             word += s[i]
     if len(word) > 0:
         mas += [word]
     return mas
+list_ = split_(string)
+count = 0
+for el in list_:
+    if len(el) !=  0;
+    count +=1
+    print (f'Количество слов в данном тексте {count}')
 
 def spaces(string): 
     i = 0
